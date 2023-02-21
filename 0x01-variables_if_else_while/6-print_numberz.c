0fdc2a0e2b6ac665facee21a220a6de2fdbce0c4
iihcub <okonkwogodspower@yahoo.com>
2023-02-21 12:48:12 -0800

    print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,12 +3,12 @@
 /**
  * main - Program starting point
  *
- * Return: ALways 0 (success)
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	char num = '0';
+	int num = '0';
 
 	for (; num <= '9'; num++)
 	{
