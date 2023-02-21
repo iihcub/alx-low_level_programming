998c90df1c2e40f13f773c02e38e89d9cba860c1
iihcub <okonkwogodspower@yahoo.com>
2023-02-21 11:55:11 -0800

    print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+	}
+
+	printf("\n");
+	return (0);
+}
